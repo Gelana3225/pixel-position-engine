0112787ef4b52bb07251935aea824dbe3082a3c6
Gelana3225 <gelogeti@gmail.com>
2025-12-28 15:25:02 -0800

    Set up frontend layout and reusable Blade components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,23 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pixel Position</title>
-    @vite(['resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@100..900&display=swap" rel="stylesheet">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white " style="font-family: 'Hanken Grotesk', sans-serif;">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
                 <a href="">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                 </a>
             </div>
 
-            <div>links</div>
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salary</a>
+                <a href="#">Company</a>
+            </div>
 
-            <div>post a job</div>
+            <div>
+                <a href="">Post a Job</a>
+            </div>
         </nav>
 
-        <main>
+        <main class="mt-10 max-w-[986px] mx-auto">
             {{ $slot }}
         </main>
     </div>
