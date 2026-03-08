0adc95ce4276a2cf2188a6611479e8f5daa2abb7
Gelana <gelogeti@gmail.com>
2026-03-08 22:23:55 +0300

    Registration page

---
+++ app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\Rules\File;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $userAttributes = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', Password::min(6)],
+        ]);
+
+        $employerAttributes = $request->validate([
+            'employer' => ['required'],
+            'logo' => ['required', File::types(['png', 'jpg', 'webp'])],
+        ]);
+
+        $user = User::create($userAttributes);
+
+        $logoPath = $request->logo->store('logos');
+
+        $user->employer()->create([
+            'name' => $employerAttributes['employer'],
+            'name' => $logoPath
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+}
